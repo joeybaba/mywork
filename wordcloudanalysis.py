b3695f414fe32a74fea47d84f81dfa89b9a4255d
joeybaba <scau2017@163.com>
2020-05-25 23:55:06 +0800

    词频词云分析

--- wordcloudanalysis.py
+++ wordcloudanalysis.py
@@ -8,6 +8,7 @@ import imageio
 import matplotlib.pyplot as plt
 
 
+# 输出词云图片
 def wordcloudanalysis():
     with open('/Users/joey/Documents/PycharmProjects/mywork/Untitled.csv', 'r') as f:
         file = f.read().lower()
@@ -27,23 +28,27 @@ def wordcloudanalysis():
     # plt.show()
 
 
+# 中文分词
 def jiebaanalysis():
     with open('/Users/joey/Documents/PycharmProjects/mywork/Untitled.csv', 'r') as f:
         file = f.read().lower()
     textlist = jieba.lcut(file)
+    # 过滤空格等无效单词
     newlist = []
-    for i, v in enumerate(textlist):
-        if v in ['\n', '-']:
+    for v in textlist:
+        if v in ['\n', '-', '']:
             continue
         else:
             newlist.append(v.strip())
+    # 计数
     countdict = {}
     for i in newlist:
         countdict[i] = countdict.get(i, 1) + 1
+
     countlist = list(countdict.items())
     countlist.sort(key=lambda x: x[1], reverse=True)
     print(type(countlist))
-    with open('countlist.text', 'w', encoding='utf-8') as f:
+    with open('countlist.txt', 'w', encoding='utf-8') as f:
         f.write('词组 ' + ' 频率\n')
         for i in countlist:
             f.write(str(i) + '\n')
@@ -52,12 +57,13 @@ def jiebaanalysis():
     return newstr
 
 
-def test(newlist):
+# jieba解释后画词云
+def test(newstr):
     font = '/Users/joey/Library/Fonts/TT-Me.ttf'
     w = wordcloud.WordCloud(width=1920, height=1080, font_path=font,
                             background_color='white', max_words=200,
-                            )
-    w.generate_from_text(newlist)
+                            scale=15)  # scale 清晰度
+    w.generate_from_text(newstr)
     w.to_file('test100.png')
 
 
